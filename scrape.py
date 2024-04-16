86d7713ab958150a3f3f47588a5635b635bb88c8
Igor Bartkowski <igorbartkowski@MacBook-Pro-Igor.local>
2024-04-16 15:32:26 +0200

    Initial commit

---
+++ scrape.py
@@ -0,0 +1,32 @@
+import requests
+from bs4 import BeautifulSoup
+import pprint
+
+page = 1
+while page != 4:
+    res = requests.get(f'https://news.ycombinator.com/news?p={page}')
+    page = page + 1
+
+soup = BeautifulSoup(res.text, 'html.parser')
+links = soup.select('.titleline > a')
+subtext = soup.select('.subtext')
+
+
+def sort_stories_by_votes(hnlist):
+    return sorted(hnlist, key=lambda k: k['votes'], reverse=True)
+
+
+def create_custom_hn(links, subtext):
+    hn = []
+    for idx, item in enumerate(links):
+        title = item.getText()
+        href = item.get('href', None)
+        vote = subtext[idx].select('.score')
+        if len(vote):
+            points = int(vote[0].getText().replace(' points', ''))
+            if points > 99:
+                hn.append({'title': title, 'link': href, 'votes': points})
+    return sort_stories_by_votes(hn)
+
+
+pprint.pprint(create_custom_hn(links, subtext))
